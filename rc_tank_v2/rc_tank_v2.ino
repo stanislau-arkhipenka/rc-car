11970e2779bac4ad8a503a3f0449448d67fb794b
Stanislau Arkhipenka <stanislau.arkhipenka@gmail.com>
2024-06-23 21:01:33 +0100

    implement arduino rc tank v2 firmware

--- rc_tank_v2/rc_tank_v2.ino
+++ rc_tank_v2/rc_tank_v2.ino
@@ -6,7 +6,7 @@
 
 //create an RF24 object
 RF24 radio(9, 8);  // CE, CSN
-#define RF_POWER RF24_PA_LOW
+#define RF_POWER RF24_PA_HIGH
 
 //address through which two modules communicate.
 const byte addresses[][6] = {"veh01","con01"};
@@ -19,37 +19,27 @@ struct c_pack {
  int b_left;
  int b_right;
  int b_joy;
- int joy_x;
- int joy_y;
+ int joy_x; // -32 .. 32
+ int joy_y; // -32 .. 32
 } pack;
 
-#define LED 13 // PIN TO SHOW Received package
+// MAX val
+#define joy_max 32 
 
-// device_id = 0 ---> speed.
-// value ---> -32 .. 0 .. 32
-#define I1 3  // Control pin 1 for motor 1 - DIGITAL
-#define I2 4  // Control pin 2 for motor 1 - DIGITAL
-#define E1 5  // Enable Pin for motor 1    - ANALOG    (PWM)
-
-// device_id = 1 ---> turn
-// value ---> -32 .. 0 .. 32
-int device_1_pin = 6; //                     DIGITAL  (PWM)
-Servo device_1;
+// ESC config
+#define ESC_LEFT_PIN 6  //    DIGITAL PWM
+#define ESC_RIGHT_PIN 9 //    DIGITAL PWM
+#define ESC_MIN_W 1000 
+#define ESC_MAX_W 2000
+Servo esc_left;
+Servo esc_right;
 
 
 void setup()
 {
   //---------------- DEBUG ---------------------------
   Serial.begin(9600);
-  // --------------- CONFIGURE DEVICE_0 --------------- 
-  pinMode(E1, OUTPUT);
-  pinMode(I1, OUTPUT);
-  pinMode(I1, OUTPUT);
-  // --------------- CONFIGURE DEVICE_1 --------------- 
-  device_1.attach(device_1_pin);
-  
-  
-  
+
   // --------------- CONFIGURE RADIO --------------- 
   radio.begin();
   radio.setPALevel(RF_POWER);
@@ -59,6 +49,11 @@ void setup()
   
   //Set module as receiver
   radio.startListening();
+
+  // --------------- CONFIGURE ESC --------------- 
+  esc_left.attach(ESC_LEFT_PIN, ESC_MIN_W, ESC_MAX_W);
+  esc_right.attach(ESC_RIGHT_PIN, ESC_MIN_W, ESC_MAX_W);
+  static int senter = int((ESC_MAX_W + ESC_MIN_W)/2);
 }
 
 void loop()
@@ -66,14 +61,35 @@ void loop()
   //Read the data if available in buffer
   if (radio.available())
   {
-    digitalWrite(LED, HIGH);
+    digitalWrite(LED_BUILTIN, HIGH);
     while(radio.available())
     {
       radio.read(&pack, sizeof(pack));
     }
     
     // do your stuff here
+    int left_value = map(min(max(pack.joy_y + pack.joy_x, joy_max), -joy_max), -joy_max, joy_max, ESC_MIN_W, ESC_MAX_W);
+    int right_value = map(min(max(pack.joy_y - pack.joy_x, joy_max), -joy_max),-joy_max, joy_max, ESC_MIN_W, ESC_MAX_W);
+    esc_left.writeMicroseconds(left_value);
+    esc_right.writeMicroseconds(right_value);
     
-    digitalWrite(LED, LOW);
+    digitalWrite(LED_BUILTIN, LOW);
   }
+}
+
+void led_delay(int t) {
+  bool on = false;
+  static int step = 100;
+  pinMode(LED_BUILTIN, OUTPUT);
+  for (int i=0;i<t;i+=step)
+  {
+    if (on == true) {
+      digitalWrite(LED_BUILTIN, HIGH);
+    } else {
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+    on = !on;
+    delay(step);
+  }
+  digitalWrite(LED_BUILTIN, LOW);
 }
